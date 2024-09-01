86a8a8dd4b409a6bd9b9301cda7e0499e0b10172
kaustav <kaustavrajkalita9925@gmail.com>
2024-09-01 23:41:54 +0530

    Added firestore, remoteVideoStream

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import VideoStream from './VideoStream';
+import { initializeApp } from "firebase/app";
 import './App.css';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDqNg0bzlWX2nLRPjzPZGeAi2FTxLJzIVs",
+    authDomain: "webrtc-vidme.firebaseapp.com",
+    projectId: "webrtc-vidme",
+    storageBucket: "webrtc-vidme.appspot.com",
+    messagingSenderId: "465596244398",
+    appId: "1:465596244398:web:9f8b8835818d31a6c1a83f",
+    measurementId: "G-P3DWGJFLVC"
+};
+
+const servers = {
+    iceServers: [
+        {
+            urls: "stun:stun.l.google.com:19302"
+        }
+    ]
+};
+
 
 
 function App() {
-    const [stream, setStream] = useState(false);
-    const handleButtonClick = async () => {
+    const [localStream, setLocalStream] = useState(false);
+
+    const handleLocalButtonClick = (action) => {
+        if (action === "start") {
+            setLocalStream(true);
+        } else {
+            setLocalStream(false);
+        }
 
     }
+
+    useEffect(() => {
+        const pc = new RTCPeerConnection(servers);
+    }, []);
+
     return (
         <div className="App">
-            <VideoStream />
+            {(localStream) ? <VideoStream /> : <p>Stream is OFF</p>}
+            <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={() => { handleLocalButtonClick("start") }}>Start Local Stream</button>
+            <button className="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded" onClick={() => { handleLocalButtonClick("stop") }}>Stop Local Stream</button>
         </div>
     );
 }
--- src/VideoStream.js
+++ src/VideoStream.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { useRef, useEffect } from 'react';
 function VideoStream() {
-    const videoRef = useRef(null);
+    let localVideoStream = null;
+    let remoteVideoStream = null;
+
+    const localVideoRef = useRef(null);
+    const remoteVideoRef = useRef(null);
 
     const hasUserMedia = () => {
         navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
@@ -11,12 +15,43 @@ function VideoStream() {
 
     const getMediaStream = async () => {
         if (hasUserMedia()) {
-            const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-            if (videoRef.current) {
-                videoRef.current.srcObject = stream;
+            localVideoStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+            remoteVideoStream = new MediaStream();
+            handlePeerConnection();
+            if (localVideoRef.current && remoteVideoRef.current) {
+                localVideoRef.current.srcObject = localVideoStream;
+                remoteVideoRef.current.srcObject = remoteVideoStream;
             }
+
         } else {
             alert("getUserMedia() is not supported in your browser");
+        }
+    }
+
+    const handlePeerConnection = () => {
+        try {
+            const pc = new RTCPeerConnection();
+            localVideoStream.getTracks().forEach(track => pc.addTrack(track, localVideoStream));
+            pc.onicecandidate = (event) => {
+                if (event.candidate) {
+                    console.log("New ICE candidate: ", event.candidate);
+                }
+            }
+            pc.ontrack = (event) => {
+                remoteVideoStream.addTrack(event.track, remoteVideoStream);
+            }
+        } catch (error) {
+            console.error("Error creating peer connection: ", error);
+        }
+        const pc = new RTCPeerConnection();
+        localVideoStream.getTracks().forEach(track => pc.addTrack(track, localVideoStream));
+        pc.onicecandidate = (event) => {
+            if (event.candidate) {
+                console.log("New ICE candidate: ", event.candidate);
+            }
+        }
+        pc.ontrack = (event) => {
+            remoteVideoStream.addTrack(event.track, remoteVideoStream);
         }
     }
 
@@ -24,8 +59,9 @@ function VideoStream() {
         getMediaStream();
     }, []);
     return (
-        <div>
-            <video ref={videoRef} autoPlay playsInline></video>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg mx-auto border">
+            <video className='container mx-auto px-4"' ref={localVideoRef} autoPlay playsInline></video>
+            <video className='container mx-auto px-4"' ref={remoteVideoRef} autoPlay playsInline></video>
         </div>
     );
 }
